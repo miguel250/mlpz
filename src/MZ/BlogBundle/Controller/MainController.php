6386c579998e191c5a0565b9ba740ebfb300fbe9
Miguel Perez <miguel250@gmail.com>
2011-08-08 19:07:24 -0400

    Show list of posts in homepage

--- src/MZ/BlogBundle/Controller/MainController.php
+++ src/MZ/BlogBundle/Controller/MainController.php
@@ -5,15 +5,19 @@ namespace MZ\BlogBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use MZ\BlogBundle\Document\Posts;
+use MZ\BlogBundle\Document\Tags;
 
 class MainController extends Controller
 {
-    /**
-     * @Route("/")
-     * @Template()
-     */
-    public function indexAction()
-    {
-        return array();
-    }
+	/**
+	 * @Route("/")
+	 * @Template()
+	 */
+	public function indexAction()
+	{
+		$dm = $this->get('doctrine.odm.mongodb.document_manager');
+		$post = $dm->getRepository('MZBlogBundle:Posts')->findAll();
+		return array('Posts'=>$post);
+	}
 }
