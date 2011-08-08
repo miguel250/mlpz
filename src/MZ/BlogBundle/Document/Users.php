e0231bba98667320d290d7a83d47803fb7c6b024
Miguel Perez <miguel250@gmail.com>
2011-08-07 20:01:19 -0400

    Added user config

---
+++ src/MZ/BlogBundle/Document/Users.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MZ\BlogBundle\Document;
+
+use FOS\UserBundle\Document\User as BaseUser;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
+/**
+ * @MongoDB\Document
+ */
+class Users extends BaseUser
+{
+	/**
+	 * @MongoDB\Id(strategy="auto")
+	 */
+	protected $id;
+
+}
