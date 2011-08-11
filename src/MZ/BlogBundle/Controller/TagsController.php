c3e2215187aef4b8258e632b8aa424e98e46763c
Miguel Perez <miguel250@gmail.com>
2011-08-11 01:07:25 -0400

    Display tags

--- src/MZ/BlogBundle/Controller/TagsController.php
+++ src/MZ/BlogBundle/Controller/TagsController.php
@@ -4,15 +4,23 @@ namespace MZ\BlogBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 
 class TagsController extends Controller
 {
 	/**
 	 * @Route("/tag/{name}")
-	 * @Template()
+	 *  @Template("SensioBlogBundle:Post:show")
 	 */
 	public function indexAction($name)
 	{
-		return array();
+		$dm = $this->get('doctrine.odm.mongodb.document_manager');
+		$tag = $dm->getRepository('MZBlogBundle:Tags')->findOneByName($name);
+		
+		if(empty($tag)){
+			throw new NotFoundHttpException("The tag couldn't be found");
+		}
+		return array('tag'=>$tag);
 	}
 }
