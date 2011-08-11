4a88773b7c36cd9ba5d90d43bfa4ffd009865299
Miguel Perez <miguel250@gmail.com>
2011-08-11 02:33:44 -0400

    Added website sitemap

--- src/MZ/BlogBundle/Controller/MainController.php
+++ src/MZ/BlogBundle/Controller/MainController.php
@@ -5,6 +5,7 @@ namespace MZ\BlogBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 use MZ\BlogBundle\Document\Posts;
 use MZ\BlogBundle\Document\Tags;
 
@@ -19,5 +20,20 @@ class MainController extends Controller
 		$dm = $this->get('doctrine.odm.mongodb.document_manager');
 		$post = $dm->getRepository('MZBlogBundle:Posts')->findAll();
 		return array('Posts'=>$post);
+	}
+	
+	/**
+	 * @Route("/sitemap.xml")
+	 * @Template("MZBlogBundle:Main:sitemap.xml.twig")
+	 */
+	public function sitemapAction()
+	{
+		$dm = $this->get('doctrine.odm.mongodb.document_manager');
+		$post = $dm->getRepository('MZBlogBundle:Posts')->findAll();
+		
+		$response =  $this->render('MZBlogBundle:Main:sitemap.xml.twig',array('Posts'=>$post));
+		$response->headers->set('Content-Type', 'text/xml');
+		
+		return $response;
 	}
 }
