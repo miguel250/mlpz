0a89b5a401b12f7defbea2176040ca26e685aa3a
Miguel Perez <miguel250@gmail.com>
2011-08-13 15:03:41 -0400

    image and video fields are not required

--- src/MZ/BlogBundle/Form/Type/NewFormType.php
+++ src/MZ/BlogBundle/Form/Type/NewFormType.php
@@ -10,8 +10,8 @@ class NewFormType extends AbstractType
 	public function buildForm(FormBuilder $builder, array $options)
 	{
 		$builder->add('title','text',array('label'=>'Post Tile'));
-		$builder->add('image','textarea',array('label'=>'Image'));
-		$builder->add('video','textarea',array('label'=>'Video'));
+		$builder->add('image','textarea',array('label'=>'Image','required'=>false));
+		$builder->add('video','textarea',array('label'=>'Video','required'=>false));
 		$builder->add('body','textarea');
 		$builder->add('type','choice',array('expanded'=> true,
 		'choices' => array('post' => 'Post','image' => 'Images', 'video' => 'Video')));
