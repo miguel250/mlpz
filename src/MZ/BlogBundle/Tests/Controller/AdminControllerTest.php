ea1816fb4a2a6d8757347fd3f2638b176e33f111
Miguel Perez <miguel250@gmail.com>
2011-09-13 00:26:39 -0400

    add title

--- src/MZ/BlogBundle/Tests/Controller/AdminControllerTest.php
+++ src/MZ/BlogBundle/Tests/Controller/AdminControllerTest.php
@@ -6,7 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class AdminControllerTest extends WebTestCase
 {
+
     public function testIndex()
     {
+       $client = self::createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+
+        $this->assertRegexp('/\d+ Bundles/', str_replace("\n", '', trim($crawler->filter('h1')->text())));
     }
+
 }
